ce5adf5520929d00565e26a998a1f794567e859c
Rachel Paulino Previtali <rachel_previtali@outlook.com>
2025-10-06 14:52:24 -0300

    código final da coleta de dados do MPU6050

--- Luva_MIDI_por_Mapeamento_Gestual.ino
+++ Luva_MIDI_por_Mapeamento_Gestual.ino
@@ -1,15 +1,25 @@
 #include "arduino_secrets.h"
 
-#include "Wire.h"  // Faz a comunicaÃ§Ã£o I2C
+#include "Wire.h"  // Faz a comunicação I2C
 #include <MPU6050_light.h>
 #include "MPU6050_data.h"
 
-MPU6050 mpu(Wire);  // Cria um objeto MPU6050 com o parÃ¢metro Wire (comunicaÃ§Ã£o I2C)
+MPU6050 mpu(Wire);  // Cria um objeto MPU6050 com o parâmetro Wire (comunicação I2C)
 unsigned long timer = 0;
+#define RED_BUILTIN 27
+#define GREEN_BUILTIN 32
+#define YELLOW_BUILTIN 33
 
 void setup() {
-  Serial.begin(9600);  // Inicia a comunicaÃ§Ã£o serial com o computador (9600 Ã© a velocidade)
-  
+  Serial.begin(115200);  // Inicia a comunicação serial com o computador
+
+  pinMode(RED_BUILTIN, OUTPUT);
+  digitalWrite(RED_BUILTIN, LOW);
+  pinMode(GREEN_BUILTIN, OUTPUT);
+  digitalWrite(GREEN_BUILTIN, LOW);
+  pinMode(YELLOW_BUILTIN, OUTPUT);
+  digitalWrite(YELLOW_BUILTIN, LOW);
+
   mpu_setCommunication();
   mpu_calibration();
 }
--- MPU6050_data.ino
+++ MPU6050_data.ino
@@ -1,40 +1,66 @@
 #include "MPU6050_data.h"
 
+const float G_TO_MS2 = 9.80665; // Conersão de g para m/s²
+
 void mpu_calibration() {
-  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  Serial.println(F("Calibrando, nao mova o mpu"));
   delay(1000);
   mpu.calcOffsets(true,true); // giroscÃ³pio and acelerÃ´metro
   Serial.println("Done!\n");
+
+  mpu.setAccConfig(2);
+  Serial.println("Faixa do acelerômetro configurada para +/- 8g");
 }
 
-void mpu_setCommunication() {
-  
+void mpu_setCommunication() { 
   Wire.begin();  
   byte status = mpu.begin();  // Inicia o sensor
   Serial.print(F("MPU6050 status: "));
   Serial.println(status);  // Status 0 indica que o sensor foi encontrado
-  while(status!=0){ } // Entra em um loop infinito atÃ© que o sensor seja encontrado
+  Serial.println("Falha ao iniciar o MPU6050. Verifique as conexões e reinicie.");
+  while(status!=0){} // Entra em um loop infinito atÃ© que o sensor seja encontrado
 }
 
 void mpu_read() {
   mpu.update();  // LÃª os dados mais recentes do sensor e os armazena dentro de mpu
+
   // Imprime os dados a cada segundo
-  if(millis() - timer > 1000){ // milis() retorna o tempo desde o inÃ­cio da execuÃ§Ã£o do cÃ³digo
-    Serial.print(F("ACELERAÃÃO  X: "));
-    Serial.print(mpu.getAccX());
+  if(millis() - timer > 1000){ // milis() retorna o tempo desde o iní­cio da execução do código
+
+    float AccX = mpu.getAccX() * G_TO_MS2;
+    Serial.print(F("ACELERACAO  X: "));
+    Serial.print(AccX);
+    float AccY = mpu.getAccY() * G_TO_MS2;
     Serial.print("\tY: ");
-    Serial.print(mpu.getAccY());
+    Serial.print(AccY);
+    float AccZ = mpu.getAccZ() * G_TO_MS2;
     Serial.print("\tZ: ");
-    Serial.println(mpu.getAccZ());  // println pula linha
+    Serial.println(AccZ);  // println pula linha
     
+    float AngleX = mpu.getAngleX();
     Serial.print(F("ANGULO  X: "));
-    Serial.print(mpu.getAngleX());
+    Serial.print(AngleX);
+    float AngleY = mpu.getAngleY();
     Serial.print("\tY: ");
-    Serial.print(mpu.getAngleY());
+    Serial.print(AngleY);
+    float AngleZ = mpu.getAngleZ();
     Serial.print("\tZ: ");
-    Serial.println(mpu.getAngleZ());
+    Serial.println(AngleZ);
     
     Serial.println(F("=====================================================\n"));
+
+    if(AngleX > 0) {
+      digitalWrite(RED_BUILTIN, HIGH);
+      digitalWrite(GREEN_BUILTIN, LOW);
+    }
+    else {
+      digitalWrite(GREEN_BUILTIN, HIGH);
+      digitalWrite(RED_BUILTIN, LOW);
+    }
+
+  if(AccX > 5 || AccX < -5) digitalWrite(YELLOW_BUILTIN, HIGH);
+
+  else digitalWrite(YELLOW_BUILTIN, LOW);
 
     timer = millis();  // Atualiza o tempo
   }
