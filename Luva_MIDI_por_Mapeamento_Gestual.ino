5373a1ab9e00f12e1114a83250e4302abd72b10e
Maria Eduarda de Gracia <144639670+MEG3832@users.noreply.github.com>
2025-10-12 21:17:55 -0300

    Adicionei um enum para as oitavas e um int binário para o sustenido

--- Luva_MIDI_por_Mapeamento_Gestual.ino
+++ Luva_MIDI_por_Mapeamento_Gestual.ino
@@ -1,29 +1,67 @@
-#include "arduino_secrets.h"
-
 #include "Wire.h"  // Faz a comunicação I2C
 #include <MPU6050_light.h>
 #include "MPU6050_data.h"
+#include <BLEMIDI_Transport.h>
+#include <hardware/BLEMIDI_ESP32.h>
+#include <map>
+#include <string.h>
 
+BLEMIDI_CREATE_DEFAULT_INSTANCE()
+
+#define SUBIR_OITAVA 12 // Precisa somar 12 para tocar a mesma nota uma oitava acima
+
+int sustenido = 1;  // Precisa somar 1 para tocar o sustenido da respectiva nota
 MPU6050 mpu(Wire);  // Cria um objeto MPU6050 com o parâmetro Wire (comunicação I2C)
-unsigned long timer = 0;
-#define RED_BUILTIN 27
-#define GREEN_BUILTIN 32
-#define YELLOW_BUILTIN 33
+std::map<string, int> notes;
+bool isConnected = false;
+
+// Denifição das notas base
+void creatNotes() {
+  notes["C1"] = 24;  // Dó
+  notes["D1"] = 26;  // Ré
+  notes["E1"] = 28;  // Mi
+  notes["F1"] = 29;  // Fá
+  notes["G1"] = 31; // Sol
+  notes["A1"] = 33; // Lá
+  notes["B1"] = 35; // Si
+}
+
+// enum das oitavas, que podem servir de multiplicador do valor a ser somado (tirei o C0 por isso)
+enum Oitavas {
+  C1, // 0
+  C2, // 1
+  C3, // 2
+  C4, // 3
+  C5, // 4
+  C6, // 5
+  C7, // 6
+  C8, // 7
+  C9  // 8
+}
+// Exemplo: Oitavas minhaOitava = C4; (minhaOitava = 3)
 
 void setup() {
-  Serial.begin(115200);  // Inicia a comunicação serial com o computador
+  Serial.begin(9600);  // Inicia a comunicação serial com o computador (9600 é a velocidade)
+  
+  createNotes();
 
-  pinMode(RED_BUILTIN, OUTPUT);
-  digitalWrite(RED_BUILTIN, LOW);
-  pinMode(GREEN_BUILTIN, OUTPUT);
-  digitalWrite(GREEN_BUILTIN, LOW);
-  pinMode(YELLOW_BUILTIN, OUTPUT);
-  digitalWrite(YELLOW_BUILTIN, LOW);
+  // Inicialização do MIDI
+  MIDI.begin();
+  BLEMIDI.setHandleConnected([]() {
+    isConnected = true;
+  });
+  BLEMIDI.setHandleDisconnected([]() {
+    isConnected = false;
+  });
 
+  // Inicialização do MPU
   mpu_setCommunication();
   mpu_calibration();
 }
 
 void loop() {
-  mpu_read();
+  MIDI.read();
+  if (isConnected) {
+    mpu_read();
+  }
 }
