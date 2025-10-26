e339cf055d9382b98b7aa1e16819b4fa5fc357e5
Maria Eduarda de Gracia <144639670+MEG3832@users.noreply.github.com>
2025-10-26 10:09:02 -0300

    Só lê os valores em x e diz se vai subir oitava e tocar sustenido

--- MPU6050_data.ino
+++ MPU6050_data.ino
@@ -1,59 +1,46 @@
 #include "MPU6050_data.h"
 
+const float G_TO_MS2 = 9.80665; // Conersão de g para m/s²
+
 void mpu_calibration() {
-  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  Serial.println(F("Calibrando, nao mova o mpu"));
   delay(1000);
-  mpu.calcOffsets(true,true); // giroscópio and acelerômetro
+  mpu.calcOffsets(true,true); // giroscÃ³pio and acelerÃ´metro
   Serial.println("Done!\n");
+
+  mpu.setAccConfig(2);
+  Serial.println("Faixa do acelerômetro configurada para +/- 8g");
 }
 
-void mpu_setCommunication() {
-  
+void mpu_setCommunication() { 
   Wire.begin();  
   byte status = mpu.begin();  // Inicia o sensor
   Serial.print(F("MPU6050 status: "));
   Serial.println(status);  // Status 0 indica que o sensor foi encontrado
-    int tentativas = 0;
-  while (status != 0 && tentativas < 10) {  // tenta no máximo 10 vezes
-    Serial.println("MPU6050 nao encontrado, tentando novamente...");
-    delay(500);  // espera meio segundo
-    status = mpu.begin();  // tenta de novo
-    tentativas++;
-  }
-
-  if (status == 0) {
-    Serial.println("MPU6050 encontrado com sucesso!");
-  } else {
-    Serial.println("Falha ao inicializar MPU6050. Continuando sem sensor..."); // Entra em um loop infinito até que o sensor seja encontrado
-  }
+  Serial.println("Falha ao iniciar o MPU6050. Verifique as conexões e reinicie.");
+  while(status!=0){} // Entra em um loop infinito atÃ© que o sensor seja encontrado
 }
 
 void mpu_read() {
-  mpu.update();  // Lê os dados mais recentes do sensor e os armazena dentro de mpu
-  
-  // Coleta os dados referentes ao eixo x
-  float AccX = mpu.getAccX() * G_TO_MS2;  
+  mpu.update();  // Le os dados mais recentes do sensor e os armazena dentro de mpu
+
+  // Leitura dos dados específicos
+  float AccX = mpu.getAccX() * G_TO_MS2;
   float AngleX = mpu.getAngleX();
 
-  // Movimentos do pulso para mudança de oitava
+  // mudança da oitava pelo angulo
   if(AngleX > 0) {
-    // MIDI.sendNoteOn(60, 100, 1)
-    Oitavas oitava = C1;
+    subirOitava = 1;
   }
   else {
-    // MIDI.sendNoteOn(120, 100, 1)
-    Oitavas oitava = C2;
+    subirOitava = 0;
   }
 
-  // Movimento horizontal para ativação de sustenido
+  // Toca sustenido quando ha uma movimentacao em x
   if(AccX > 5 || AccX < -5) {
-    // MIDI.sendNoteOn(30, 100, 1)
     sustenido = 1;
   }
   else {
     sustenido = 0;
   }
-
-  // Soma a oitava e o sustenido (se não tiver, soma 0)
-  MIDI.sendNoteOn(notes["C4"] + SUBIR_OITAVA*oitava + sustenido, 100, 1);
 }
